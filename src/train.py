72131c36c26bbd16f7da64ecf596eb77fb0453b9
jkoicata <jesicajkoicata@gmail.com>
2026-08-01 09:05:59 -0500

    Update train.py

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from data import cargar_datos, separar_variables
+
 import pandas as pd
 
 def entrenar():
